79d65ce6b4807510eb2e7a0b528e7086a0f1deaa
vani8987 <vano-smole@mail.ru>
2024-11-26 17:58:40 +0700

    make_AboutMe_And_ContactInformaton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Index";
+import Main from "./components/Main";
 
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Main />
     </div>
   );
 }
